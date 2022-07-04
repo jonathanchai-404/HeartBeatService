e6ad986f4376f6306ebe14efa2a0b74ef27d6fed
jonathanchai-404 <104407482+jonathanchai-404@users.noreply.github.com>
2022-07-04 14:30:08 +0800

    Initial Commit

---
+++ HeartBeatService/Heartbeat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+
+namespace HeartBeatService
+{
+    class Heartbeat
+    {
+        private readonly Timer _timer;
+
+        public Heartbeat()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString()};
+            File.AppendAllLines(Path.Combine(Environment.CurrentDirectory, "heartbeat.txt"), lines);
+        }
+
+        public void Start() 
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ HeartBeatService/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Topshelf;
+
+/// <summary>
+/// To run as window service, input "HeartBeatService.exe install start" in cmd
+/// To uninstall, input "HeartBeatService.exe uninstall" in cmd
+/// 
+/// Service will generate a .txt file in project output folder 
+/// </summary>
+namespace HeartBeatService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitcode = HostFactory.Run(x =>
+            {
+                x.Service<Heartbeat>(s =>
+                {
+                    s.ConstructUsing(heartbeat => new Heartbeat());
+                    s.WhenStarted(heartbeat => heartbeat.Start());
+                    s.WhenStopped(heartbeat => heartbeat.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("HeartbeatService");
+                x.SetDisplayName("Heartbeat Service");
+                x.SetDescription("Sample heartbeat service");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitcode, exitcode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
